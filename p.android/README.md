657fa9075f94c420abf21f7cd0974e2febc7de15
HYbutterfly <707298413@qq.com>
2021-02-25 19:18:12 +0800

    add files to help android build

---
+++ p.android/README.md
@@ -0,0 +1,13 @@
+# Android Studio Project
+```
+	to build Android Game, you shoud create a native project, 
+	the `AndroidMainifest.xml CMakeLists.txt build.gradle` files maybe to help you
+```
+
+
+# Assets
+```
+	you shoud mkdir `kt_assets` in assets
+	put the `kite`(engine lua lib) and main.lua, config.lua and other directory(eg: `images`, `fonts`) to `kt_assets` 
+	and edit the asset_list.h (every one directory should be one item of ASSERT_DIR)
+```
--- p.android/asset_list.h
+++ p.android/asset_list.h
@@ -5,15 +5,15 @@
 
 
 
-
+// mkdir `kt_assets` in assets(android studio project), put-in `kite` and main.lua to `kt_assets`
 const char *ASSERT_DIR[] = {
     "kt_assets",
     "kt_assets/kite",
     "kt_assets/kite/manager",
     "kt_assets/kite/shader",
-    "kt_assets/ui",
-    "kt_assets/ui/node",
-    "kt_assets/ui/system",
+    "kt_assets/kite/ui",
+    "kt_assets/kite/ui/node",
+    "kt_assets/kite/ui/system",
     "kt_assets/image",
     "kt_assets/font",
     "kt_assets/music"
